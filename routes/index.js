e0dfa0782ed242bbc68e6ead399d6ea36977ca45
Aaron Sullivan <aasullivan1618@gmail.com>
2013-12-28 11:56:07 -0500

    asdf

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@
 
 exports.index = function(req, res){
 	var min = (req.app.get('env') == 'production') ? ".min" : "";
+	console.log('min')
+	console.log(min)
 
   res.render('index', { title: "Aaronik's Asteroids", min: min });
 };
